30f1dafcab1ec9e0dce37cf15aa6fa661d963153
Ashok Shah <bayes.ashok@gmail.com>
2024-12-11 21:43:52 +0545

    common snackbar added to project

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shikshalaya/common/common_snackbar.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,6 +11,23 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("login"),
+      ),
+      body: SafeArea(
+        child: ElevatedButton(
+          onPressed: () {
+            CommonSnackbar.show(
+              context,
+              message: "This is a common snackbar!",
+              backgroundColor: Colors.blue,
+              textColor: Colors.white,
+            );
+          },
+          child: const Text("Button 1"),
+        ),
+      ),
+    );
   }
 }
