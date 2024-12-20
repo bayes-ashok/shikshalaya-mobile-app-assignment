5589d60231e8ca87e95ffcdee83d7d4a5ed3556a
Ashok Shah <bayes.ashok@gmail.com>
2024-12-20 18:47:28 +0545

    added themedata

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shikshalaya/common/bottom_nav.dart';
-import 'package:shikshalaya/view/home_page.dart';
+// import 'package:shikshalaya/view/home_page.dart';
 import 'package:shikshalaya/view/login_view.dart';
+import 'package:shikshalaya/view/onboarding_screen.dart';
 // import 'package:shikshalaya/view/onboarding_screen.dart';
 import 'package:shikshalaya/view/register_view.dart';
 
@@ -13,11 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 248, 246, 246),
+      ),
       routes: {
-        '/': (context) => const BottomNav(),
+        '/': (context) => const OnboardingScreen(),
         '/register': (context) => const RegisterView(),
         '/login': (context) => const LoginView(),
-        '/dashboard': (context) => const HomePage(),
+        '/dashboard': (context) => const BottomNav(),
       },
     );
   }
