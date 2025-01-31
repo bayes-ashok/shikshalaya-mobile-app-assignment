4edf60f5b7edfb4b650ca3655c9281c2de2eb823
Ashok Shah <bayes.ashok@gmail.com>
2025-01-31 09:38:15 +0545

    added upload image use case

--- lib/features/auth/domain/use_case/register_user_usecase.dart
+++ lib/features/auth/domain/use_case/register_user_usecase.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:shikshalaya/app/usecase/usecase.dart';
@@ -43,5 +45,25 @@ class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
       password: params.password,
     );
     return repository.registerStudent(authEntity);
+  }
+}
+
+class UploadImageParams {
+  final File file;
+
+  const UploadImageParams({
+    required this.file,
+  });
+}
+
+class UploadImageUsecase
+    implements UsecaseWithParams<String, UploadImageParams> {
+  final IAuthRepository _repository;
+
+  UploadImageUsecase(this._repository);
+
+  @override
+  Future<Either<Failure, String>> call(UploadImageParams params) {
+    return _repository.uploadProfilePicture(params.file);
   }
 }
