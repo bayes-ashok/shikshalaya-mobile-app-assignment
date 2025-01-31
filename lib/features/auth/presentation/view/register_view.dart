eeaa6e7aa4dc16aacb8263bd893b09125f35f5be
Ashok Shah <bayes.ashok@gmail.com>
2025-01-31 09:36:50 +0545

    added permission

--- lib/features/auth/presentation/view/register_view.dart
+++ lib/features/auth/presentation/view/register_view.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../view_model/signup/register_bloc.dart';
 
@@ -19,18 +20,33 @@ class _RegisterViewState extends State<RegisterView> {
   final TextEditingController _phoneNumberController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
-  TextEditingController();
+      TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
-  XFile? _profileImage;
+  Future<void> checkCameraPermission() async {
+    if (await Permission.camera.request().isRestricted ||
+        await Permission.camera.request().isDenied) {
+      await Permission.camera.request();
+    }
+  }
 
-  Future<void> _pickImage() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-    if (image != null) {
-      setState(() {
-        _profileImage = image;
-      });
+  File? _img;
+  Future _browseImage(ImageSource imageSource) async {
+    try {
+      final image = await ImagePicker().pickImage(source: imageSource);
+      if (image != null) {
+        setState(() {
+          _img = File(image.path);
+
+          context.read<RegisterBloc>().add(
+                LoadImage(file: _img!),
+              );
+        });
+      } else {
+        return;
+      }
+    } catch (e) {
+      debugPrint(e.toString());
     }
   }
 
@@ -70,16 +86,55 @@ class _RegisterViewState extends State<RegisterView> {
                       ),
                     ),
                     const SizedBox(height: 15),
-                    GestureDetector(
-                      onTap: _pickImage,
-                      child: CircleAvatar(
-                        radius: 60,
-                        backgroundImage: _profileImage != null
-                            ? FileImage(File(_profileImage!.path))
-                            : const AssetImage(
-                          'assets/images/Sample_User_Icon.png',
-                        ) as ImageProvider,
-                        child: null,
+                    InkWell(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: Colors.grey[300],
+                          context: context,
+                          isScrollControlled: true,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(20),
+                            ),
+                          ),
+                          builder: (context) => Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    checkCameraPermission();
+                                    _browseImage(ImageSource.camera);
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.camera),
+                                  label: const Text('Camera'),
+                                ),
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    _browseImage(ImageSource.gallery);
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.image),
+                                  label: const Text('Gallery'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      child: SizedBox(
+                        height: 200,
+                        width: 200,
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _img != null
+                              ? FileImage(_img!)
+                              : const NetworkImage(
+                                      'https://cdn-icons-png.freepik.com/512/5178/5178994.png')
+                                  as ImageProvider,
+                        ),
                       ),
                     ),
                     const SizedBox(height: 20),
@@ -88,7 +143,7 @@ class _RegisterViewState extends State<RegisterView> {
                       decoration: InputDecoration(
                         labelText: 'Full Name',
                         prefixIcon:
-                        const Icon(Icons.person, color: Colors.blue),
+                            const Icon(Icons.person, color: Colors.blue),
                         filled: true,
                         fillColor: Colors.white,
                         border: OutlineInputBorder(
@@ -183,7 +238,7 @@ class _RegisterViewState extends State<RegisterView> {
                       decoration: InputDecoration(
                         labelText: 'Confirm Password',
                         prefixIcon:
-                        const Icon(Icons.lock_outline, color: Colors.blue),
+                            const Icon(Icons.lock_outline, color: Colors.blue),
                         filled: true,
                         fillColor: Colors.white,
                         border: OutlineInputBorder(
@@ -208,14 +263,14 @@ class _RegisterViewState extends State<RegisterView> {
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
                             context.read<RegisterBloc>().add(
-                              RegisterStudent(
-                                context: context,
-                                fName: _fullNameController.text,
-                                phone: _phoneNumberController.text,
-                                email: _emailController.text,
-                                password: _passwordController.text,
-                              ),
-                            );
+                                  RegisterStudent(
+                                    context: context,
+                                    fName: _fullNameController.text,
+                                    phone: _phoneNumberController.text,
+                                    email: _emailController.text,
+                                    password: _passwordController.text,
+                                  ),
+                                );
                           }
                         },
                         style: ElevatedButton.styleFrom(
