f9cdc2862dc2f8aa74cc2d0e41d1dd89622ba09e
Ashok Shah <bayes.ashok@gmail.com>
2025-02-07 19:25:07 +0545

    login usecase test done

---
+++ test/features/auth/domain/use_case/login_usecase_test.dart
@@ -0,0 +1,58 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:shikshalaya/core/error/failure.dart';
+import 'package:shikshalaya/features/auth/domain/use_case/login_usecase.dart';
+
+import 'repository.mock.dart';
+
+void main() {
+  late MockAuthRepository repository;
+  late LoginUseCase usecase;
+
+  setUp(() {
+    repository = MockAuthRepository();
+    usecase = LoginUseCase(repository);
+  });
+
+  const email = "testing@gmai.com";
+  const password = "password123";
+  const loginParams = LoginParams(email: email, password: password);
+
+  test('should login user successfully', () async {
+    // Arrange
+    when(() => repository.loginStudent(any(), any())).thenAnswer(
+      (_) async => const Right('token'),
+    );
+
+    // Act
+    final result = await usecase(loginParams);
+
+    // Assert
+    expect(result, const Right('token'));
+
+    // Verify repository interaction
+    verify(() => repository.loginStudent(email, password)).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+
+  test('should return failure when login fails', () async {
+    // Arrange
+    when(() => repository.loginStudent(any(), any())).thenAnswer(
+      (_) async =>
+          const Left(ApiFailure(message: 'Incorrect login credentials')),
+    );
+
+    // Act
+    final result = await usecase(loginParams);
+
+    // Assert
+    expect(
+      result,
+      const Left(ApiFailure(message: 'Incorrect login credentials')),
+    );
+
+    verify(() => repository.loginStudent(email, password)).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+}
