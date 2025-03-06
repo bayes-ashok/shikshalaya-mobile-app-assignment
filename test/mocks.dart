e73a1277d0cda43713aeaeadd09c02b6872f4ba3
Ashok Shah <bayes.ashok@gmail.com>
2025-03-06 15:14:14 +0545

    bloc test completed

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shikshalaya/app/shared_prefs/token_shared_prefs.dart';
-import 'package:shikshalaya/features/auth/domain/use_case/login_usecase.dart';
-import 'package:shikshalaya/features/auth/presentation/view_model/signup/register_bloc.dart';
 import 'package:shikshalaya/features/course/domain/repository/course_repository.dart';
 import 'package:shikshalaya/features/course/data/data_source/remote_datasource/course_remote_datasource.dart';
 import 'package:shikshalaya/features/course/domain/use_case/get_course_by_id_usecase.dart';
 import 'package:shikshalaya/features/course/domain/use_case/get_student_course_usecase.dart';
 import 'package:shikshalaya/features/course/domain/use_case/is_enrolled_usecase.dart';
-import 'package:shikshalaya/features/home/presentation/view_model/cubit/home_cubit.dart';
 import 'package:shikshalaya/features/payment/data/data_source/remote_data_source/payment_remote_data_source.dart';
 import 'package:shikshalaya/features/payment/domain/repository/payment_repository.dart';
 import 'package:shikshalaya/features/payment/presentation/view_model/payment_bloc.dart';
@@ -16,6 +14,8 @@ import 'package:shikshalaya/features/test/data/data_source/remote_datasource/qui
 import 'package:shikshalaya/features/test/domain/repository/quiz_repository.dart';
 import 'package:shikshalaya/features/test/domain/use_case/get_questions_usecase.dart';
 import 'package:shikshalaya/features/test/domain/use_case/get_quiz_sets_usecase.dart';
+import 'package:shikshalaya/features/user_profile/domain/use_case/get_current_user_usecase.dart';
+import 'package:shikshalaya/features/user_profile/domain/use_case/update_user_profile_usecase.dart';
 
 // -----------------------FOR UNIT TESTING--------------------------------------//
 
@@ -36,13 +36,6 @@ class MockQuizRepository extends Mock implements QuizRepository {}
 class MockQuizRemoteDataSource extends Mock implements QuizRemoteDataSource {}
 
 // ----------------------FOR BLOC TESTING -----------------------------------------//
-class MockLoginUseCase extends Mock implements LoginUseCase {}
-
-class MockRegisterBloc extends Mock implements RegisterBloc {}
-
-class MockHomeCubit extends Mock implements HomeCubit {}
-
-class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 class MockBuildContext extends Mock implements BuildContext {}
 
@@ -58,3 +51,10 @@ class MockPaymentBloc extends Mock implements PaymentBloc {}
 class MockGetQuizSetsUseCase extends Mock implements GetQuizSetsUseCase {}
 
 class MockGetQuestionsUseCase extends Mock implements GetQuestionsUseCase {}
+
+class MockGetCurrentUserUseCase extends Mock implements GetCurrentUserUseCase {}
+
+class MockUpdateUserProfileUseCase extends Mock
+    implements UpdateUserProfileUseCase {}
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
